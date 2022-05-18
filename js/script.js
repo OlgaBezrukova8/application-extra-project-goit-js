1c0ff641e5fd72c0937d82317d564e11a74e066f
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-05-18 19:30:22 +0100

    Added basic code

--- js/script.js
+++ js/script.js
@@ -41,52 +41,159 @@ const books = [
 
 const rootEl = document.querySelector("#root");
 
-const leftDiv = document.createElement("div");
-leftDiv.classList.add("list__div");
+const leftSide = document.createElement("div");
+leftSide.classList.add("left-side");
 
-const rightDiv = document.createElement("div");
-rightDiv.classList.add("right__div");
+const rightSide = document.createElement("div");
+rightSide.classList.add("right-side");
 
-const headEl = document.createElement("h1");
-headEl.textContent = "Books list";
+const title = document.createElement("h1");
+title.classList.add("title");
+title.textContent = "Books title";
 
-const listEl = document.createElement("ul");
-listEl.classList.add("book__list");
+const booksList = document.createElement("ul");
+booksList.classList.add("books-list");
 
-const btnEl = document.createElement("button");
-btnEl.textContent = "Add new book";
+const button = document.createElement("button");
+button.classList.add("books-button");
+button.textContent = "Add new book";
 
-rootEl.append(leftDiv, rightDiv);
-leftDiv.append(headEl, listEl, btnEl);
+rootEl.append(leftSide, rightSide);
+leftSide.append(title, booksList, button);
 
-const rightSide = document.querySelector(".right__div");
-const bookList = document.querySelector("book__list");
+const rightDiv = document.querySelector(".right-side");
+const booksListAdd = document.querySelector(".books-list");
 
-function renderList() {
-  const listMarkup = books
+const renderPreviewMarkup = (books) => {
+  return `
+  <div class="book-container">
+        <h2 class="book-name-title">${books.title}</h2>
+        <p class="author-name">${books.author}</p>
+        <img src="${books.img}" alt="" class="book-image">
+        <p class="book-discription">${books.plot}</p>
+  </div>`;
+};
+
+const renderPreview = (event) => {
+  const book = books.find((book) => book.title === event.target.textContent);
+  
+  rightDiv.innerHTML = "";
+  rightDiv.insertAdjacentHTML("beforeend", renderPreviewMarkup(book));
+};
+
+const deleteF = () => {
+  // const book = books.find(book => book.id === event.target.parentNode.id);
+  // console.log(book);
+};
+const editF = (event) => {
+  const book = books.find((book) => book.id === event.target.parentNode.id);
+  console.log(book);
+};
+
+const renderList = () => {
+  const booksArray = books
     .map((book) => {
       return `
-      <li class="book__item">
-        <p class="book__title">${book.title}</p>
-            <button class="edit__btn">Edit</button>
-            <button class"delete__btn">Delete</button>
+      <li id="${book.id}" class="book-item">
+        <p class="book-title">${book.title}</p>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
       </li>`;
     })
     .join("");
 
-  document;
-  bookList.insertAdjacentHTML("beforeend", listMarkup);
+  booksListAdd.insertAdjacentHTML("beforeend", booksArray);
 
-  const p = document.querySelector(".book__title");
-  p.forEach((el) => el.addEventListener("click", renderPrewiew));
-  document.querySelector(".edit__btn").forEach(el => el.addEventListener("click", ))
-}
+  const p = document.querySelectorAll(".book-title");
+  p.forEach((el) => el.addEventListener("click", renderPreview));
 
-const renderPrewiew = (event) => {
-  const book = books.find(
-    (book) => book.title === event.currentTarget.textContent
-  );
-  rightSide.innerHTML("");
+  document
+    .querySelectorAll(".edit-btn")
+    .forEach((el) => el.addEventListener("click", editF));
+
+  document
+    .querySelectorAll(".delete-btn")
+    .forEach((el) => el.addEventListener("click", deleteF));
 };
 
 renderList();
+
+// const rootEl = document.querySelector("#root");
+
+// const leftDiv = document.createElement("div");
+// leftDiv.classList.add("left__div");
+
+// const rightDiv = document.createElement("div");
+// rightDiv.classList.add("right__div");
+
+// const title = document.createElement("h1");
+// title.classList.add("book__title");
+// title.textContent = "Books title";
+
+// const listEl = document.createElement("ul");
+// listEl.classList.add("book__list");
+
+// const btnEl = document.createElement("button");
+// btnEl.textContent = "Add new book";
+
+// rootEl.append(leftDiv, rightDiv);
+// leftDiv.append(title, listEl, btnEl);
+
+// const rightSide = document.querySelector(".right__div");
+// const bookList = document.querySelector(".book__list");
+
+// const renderPreviewMarkup = (books) => {
+//   return `
+//   <div class="book-discr-container">
+//     <h2 class="book-disc-title">${books.title}</h2>
+//     <p class="author-name">${books.author}</p>
+//     <img src="${books.img}" alt="" class="book-image">
+//     <p class="book-discr">${books.plot}</p>
+//   </div>`;
+// };
+
+// const renderPrewiew = (event) => {
+//   const book = books.find(
+//     (book) => book.title === event.currentTarget.textContent
+//   );
+//   rightSide.innerHTML("");
+//   rightSide.insertAdjacentElement("beforeend", renderPreviewMarkup);
+// };
+
+// const editF = (event) => {
+//   const book = books.find((book) => book.id === event.target.parentNode.id);
+//   // console.log(book);
+// };
+
+// const deleteF = (event) => {
+//   // const book = books.find((book) => book.id === event.target.parentNode.id);
+//   // console.log(book);
+// };
+
+// const renderList = () => {
+//   const listMarkup = books
+//     .map((book) => {
+//       const liEl = `
+//       <li id="${book.id}" class="book__item">
+//         <p class="book__title">${book.title}</p>
+//             <button class="edit__btn">Edit</button>
+//             <button class="delete__btn">Delete</button>
+//       </li>`;
+//       return liEl;
+//     })
+//     .join("");
+
+//   bookList.insertAdjacentHTML("beforeend", listMarkup);
+
+//   const p = document.querySelector(".book__title");
+//   p.forEach((el) => el.addEventListener("click", renderPrewiew));
+//   document
+//     .querySelector(".edit__btn")
+//     .forEach((el) => el.addEventListener("click", editF));
+
+//   document
+//     .querySelectorAll(".delete-btn")
+//     .forEach((el) => el.addEventListener("click", deleteF));
+// };
+
+// renderList();
